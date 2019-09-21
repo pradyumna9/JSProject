964c9750b543c2ee1ec4d40cdf50447a3b4d1d6f
PRADYUMNA KUMAR DAS <tech.daspk@gmail.com>
2019-09-21 20:45:21 +0530

    First Commit to added the js file

---
+++ script.js
@@ -0,0 +1,16 @@
+/***************** simple example
+var firstName = "Pradyumna";
+console.log("Hello "+firstName);
+
+*/
+
+//variable and data type in js
+var firstName = "pradyumna";
+var age = 28;
+var isMarried = false;
+var height;
+height = 6.7
+var lastName = prompt("Enter your last name:");
+alert("Name of the Applicant:"+firstName+" "+lastName+" having age "+age+" is Married?"+isMarried+" height:"+height)
+
+//console.log("Name of the Applicant:"+firstName+" having age "+age+" is Married?"+isMarried+" height:"+height);
