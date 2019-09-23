582279686afd7917c23d8368f3fd9085920791d2
PRADYUMNA KUMAR DAS <tech.daspk@gmail.com>
2019-09-23 22:15:51 +0530

    Function code added in jS

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ console.log(typeof trip);
 /*
 Code Challenge 1
 */
+/*
 var markMass,johnMass,markHeight,johnHeight;
 markMass = 65;
 johnMass = 75;
@@ -62,3 +63,72 @@ console.log('mark BMI:'+markBMI);
 var johnBMI = (johnMass/(johnHeight*johnHeight));
 console.log('John BMI:'+johnBMI);
 console.log("Is Mark's BMI higher than JOHNS?"+(markBMI>johnBMI));
+*/
+
+/********CODE CHALLENGE 2
+ * 
+ */
+/*
+var johnsAverageScore = (89 + 120 + 103) / 3;
+var mikeAvergaeScore = (116 + 94 + 123) / 3;
+var maryaverage  = (97 + 134 + 105)/3
+console.log(johnsAverageScore,mikeAvergaeScore,maryaverage);
+if(johnsAverageScore>mikeAvergaeScore && johnsAverageScore>maryaverage){
+    console.log("John higest avg score");
+}
+else if(mikeAvergaeScore>johnsAverageScore && mikeAvergaeScore>maryaverage){
+    console.log("Mike highest Avg score");
+}else if(maryaverage>johnsAverageScore && maryaverage>mikeAvergaeScore){
+    console.log("Mary Highest Avg score");
+}
+else{
+    console.log("Match Drawn");
+}
+*/
+/*
+if((johnsAverageScore > mikeAvergaeScore)){
+    console.log("John Team is the winner");
+}else if(johnsAverageScore < mikeAvergaeScore){
+    console.log("Mike Having highest avg score");
+}
+else{
+    console.log("Match Drawn");
+}
+*/
+
+/***********************
+ * FUNCTIONS IN JS
+ */
+
+ function calculateAge(birthyear){
+     return 2019-birthyear;
+ }
+console.log("Age of Dipu:"+calculateAge(1991));
+console.log("Age of Baby:"+calculateAge(1990));
+
+function yearUntilretirement(birthyear,firstName){
+    var age = 2019-birthyear;
+    retireyear= 65-age;
+    console.log(firstName+" years left for retirement:"+retireyear+" years");
+}
+yearUntilretirement(1991,"Pradyumna");
+yearUntilretirement(1990,"Itishree");
+
+var profession = function(job,firstName){
+    switch(job){
+        case 'Teacher':
+            console.log(firstName+' knows how to teach code');
+            break;
+        case 'Barber':
+            console.log(firstName+' knows how to cut the hair');
+            break;
+        case 'Carpenter':
+            console.log(firstName+' knows how to design wooden stuff');
+            break;
+        default:
+            console.log(firstName+' is Jobless need a job');
+    }
+}
+
+profession('Teacher','Pradyumna');
+profession('Carpenter','Itishree');
